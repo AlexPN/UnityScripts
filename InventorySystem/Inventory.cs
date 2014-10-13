5165c5aa2c0946dde83e84c7f62a0961277d6017
Alexandr P. Nazartsev <alex.nazartsev@gmail.com>
2014-10-13 16:50:27 -0400

    Converting Dictionary to Array

--- InventorySystem/Inventory.cs
+++ InventorySystem/Inventory.cs
@@ -11,6 +11,8 @@ public class Inventory : MonoBehaviour {
 	private int SortType = 0;
 	private string[] SortTypeList = new string[] {"Name","Quantity"};
 
+	public int SelectedItem;
+
 	void Start () {
 
 	}
@@ -38,29 +40,33 @@ public class Inventory : MonoBehaviour {
 			GUI.Label(new Rect(32,32,64,24), "Sort by:");
 			SortType = GUI.Toolbar(new Rect(96,32,128,24), SortType,SortTypeList);
 			if (InvDictionary.Count > 0) {
+				GameObject[] objs = new GameObject[] { };
 				if (SortType == 0) {
 					//Items ordered by name
-					string[] test = new string[InvDictionary.Count];
-					foreach (KeyValuePair<GameObject,int> item in InvDictionary.OrderBy(key => key.Key)) {
+					InvDictionary.OrderBy(key => key.Key);
+					//foreach (KeyValuePair<GameObject,int> item in InvDictionary.OrderBy(key => key.Key)) {
 						/*
 						item_base ObjItem = this.gameObject.AddComponent("item_base");
 						ObjItem = item.Key.GetComponent("item_base") as item_base;
 						Texture2D DispTex = ObjItem.InvTexture;
 						GUILayout.Button(DispTex);
 						*/
-					}
+					//}
 				}
 				else {
 					//Items ordered by quantity
-					foreach (KeyValuePair<GameObject,int> item in InvDictionary.OrderBy(key => key.Value)) {
+					InvDictionary.OrderBy(key => key.Value);
+					//foreach (KeyValuePair<GameObject,int> item in InvDictionary.OrderBy(key => key.Value)) {
 						/*
 						item_base ObjItem = this.gameObject.AddComponent("item_base");
 						ObjItem = item.Key.GetComponent("item_base") as item_base;
 						Texture2D DispTex = ObjItem.InvTexture;
 						GUILayout.Button(DispTex);
 						*/
-					}
+					//}
 				}
+				InvDictionary.Keys.CopyTo(objs,0);
+				//SelectedItem = GUI.SelectionGrid(new Rect(),SelectedItem,objs.ToString(),5);
 			}
 			GUI.EndGroup();
 		}
